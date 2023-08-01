3a01e537dc8f65c6368cf54bc8a0593e1d35a327
azhnhakimi <azhanhakimii@gmail.com>
2023-08-01 20:49:33 +0800

    first attempt at using geocoding api and axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,23 @@
 import "./App.css";
+import axios from "axios";
+import { useEffect } from "react";
 
 function App() {
+	useEffect(() => {
+		axios
+			.get(
+				"http://api.openweathermap.org/geo/1.0/direct?q=Kuala Lumpur&limit=5&appid=fabd0680cafbb21a09efe3f07d816826"
+			)
+			.then((res) => showResults(res.data))
+			.catch((err) => console.log(err));
+	}, []);
+
+	const showResults = (data) => {
+		data.forEach((element) => {
+			console.log(element);
+		});
+	};
+
 	return (
 		<>
 			<div className="mainContainer"></div>
